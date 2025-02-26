36b26bc6275fb5111213553ff15450094e07a9b0
fazategarb <legenddesign01@gmail.com>
2025-02-26 18:36:18 +0700

    add change hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,13 +3,20 @@ import imgCover from '../assets/cover.jpg';
 
 const Hero = () => {
   return (
-    <section id="hero" className="h-full items-center justify-center mx-auto max-w-2xl mt-25 mb-10">
-      <img src={imgCover} alt="cover" className=' w-full h-40 rounded-2xl object-cover overflow-hidden mb-15 animate-delay [--delay:0s]'/>
-      <h1 className="text-2xl font-bold animate-delay [--delay:0.2s]">Hi, I'm Faza Tegar Balintra</h1>
-      <p className="text-lg mt-4 animate-delay [--delay:0.5s]">A Full Stack Developer with a passion for Web Technologies.</p>
-      <button className=' bg-white text-black mt-6 px-6 py-2 rounded-lg animate-delay [--delay:0.7s]'>
-      <a href="/projects">View My Works</a>
-      </button>
+    <section id="hero" className="h-full items-center justify-center mx-auto max-w-2xl mt-20 mb-20">
+      
+      {/* image cover */}
+      <img src={imgCover} alt="cover" className=' w-full h-50 rounded-2xl object-cover overflow-hidden mb-15 animate-delay [--delay:0s]'/>
+
+      {/* introducing website */}
+      <h1 className="text-2xl font-semibold animate-delay [--delay:0.2s]">Hi, I'm <span className='hover:text-orange-200 transition-color duration-300'>Faza Tegar Balintra</span></h1>
+      <p className="text-lg my-5 animate-delay [--delay:0.5s]">Welcome to my portofolio, friends!</p>
+      <p className='text-lg my-5 animate-delay [--delay:0.8s]'>This website might be for share my experience and projects for future use. Planning so share cool projects I've created complete with case of study. Wanna 'chit chat' to me? you can follow or hangout there with me. Let me know for future colaborations with me, stay tune!</p>
+      <p className='text-lg my 5 animate-delay [--delay:1.1s]'>Keep in mind, this website is still under development, stay with me for updated contents!/</p>
+
+      {/* about me */}
+      
+
     </section>
   )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <nav className="bg-[#161616] text-white py-4 flex justify-between mx-auto max-w-2xl fixed top-0 left-0 right-0 z-10">
-      <h1 className="text-2xl font-bold"><a href="/">fazadev</a><span className='text-2xl font-medium text-blue-500'>_</span></h1>
+    <nav className="bg-[#161a1d]/50 text-white py-4 flex justify-between mx-auto max-w-2xl fixed top-0 left-0 right-0 z-10 backdrop-blur-lg rounded">
+      <h1 className="text-2xl font-bold"><a href="/">fazadev</a><span className='text-xl font-bold text-blue-500'>.</span></h1>
       <ul className="flex gap-7">
-        <li className='hover:scale-110 hover:transition-transform'><a href="#hero" className='hover:text-blue-500 hover:transition-colors'>Home</a></li>
-        <li className='hover:scale-110 hover:transition-transform'><a href="#projects" className='hover:text-blue-500 hover:transition-colors'>Projects</a></li>
-        <li className='hover:scale-110 hover:transition-transform'><a href="#contact" className='hover:text-blue-500 hover:transition-colors'>Contact</a></li>
+        <li><a href="/" className='hover:text-blue-500 hover:transition-colors text-lg'>Home</a></li>
+        <li><a href="/#projects" className='hover:text-blue-500 hover:transition-colors text-lg'>Projects</a></li>
+        <li><a href="/#contact" className='hover:text-blue-500 hover:transition-colors text-lg'>Contact</a></li>
       </ul>
     </nav>
   )
