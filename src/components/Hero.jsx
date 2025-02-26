eb9458a2ca46e41d77f1034d0072481eadd1ddb0
fazategarb <legenddesign01@gmail.com>
2025-02-26 13:04:52 +0700

    add change commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import imgCover from '../assets/cover.jpg';
 
 const Hero = () => {
   return (
-    <section id="hero" className="h-full items-center justify-center mx-auto max-w-2xl mt-25 mb-25">
+    <section id="hero" className="h-full items-center justify-center mx-auto max-w-2xl mt-25 mb-10">
       <img src={imgCover} alt="cover" className=' w-full h-40 rounded-2xl object-cover overflow-hidden mb-15 animate-delay [--delay:0s]'/>
       <h1 className="text-2xl font-bold animate-delay [--delay:0.2s]">Hi, I'm Faza Tegar Balintra</h1>
       <p className="text-lg mt-4 animate-delay [--delay:0.5s]">A Full Stack Developer with a passion for Web Technologies.</p>
