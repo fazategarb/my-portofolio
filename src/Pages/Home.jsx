fc6233442acb32ddaac971d3727d3418b14b7f66
fazategarb <legenddesign01@gmail.com>
2025-02-28 23:55:00 +0700

    Updated Pages

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
 import Footer from '../components/Footer'
+import Hero from '../components/Hero'
 
 const Home = () => {
   return (
     <>
         <NavBar/>
+        <Hero/>
         <Footer/>
     </>
   )
--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -5,8 +5,8 @@ import Footer from '../components/Footer'
 const Projects = () => {
   return (
     <>
-    <NavBar/>
-    <Footer/>
+        <NavBar/>
+        <Footer/>
     </>
   )
 }
