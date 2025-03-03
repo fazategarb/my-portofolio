29841bbc726033749de0ef2f51a6c52b614567a8
fazategarb <legenddesign01@gmail.com>
2025-03-03 13:46:32 +0700

    feat: updated files commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './Pages/Home'
-import Experience from './Pages/Experience'
-import Projects from './Pages/Projects'
-import Error from './components/Error'
+import React, { useState } from 'react'
+import LoadingScreen from './components/LoadingScreen'
+import MobileMenu from './components/MobileMenu'
+// import Home from './components/pages/Home'
+// import Experience from './components/pages/Experience'
+// import Projects from './components/pages/Projects'
+// import Error from './components/Error'
+import NavBar from './components/NavBar'
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [menuOpen, setMenuOpen] = useState(false)
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/work" element={<Experience />} />
-        <Route path="/project" element={<Projects />} />
-        <Route path="*" element={<Error/>}/>
-      </Routes>
-    </Router>
+    <>
+    {!isLoaded ? (
+      <LoadingScreen onComplete = {() => setIsLoaded(true)}/>
+      
+    ) : (
+      <>
+        <NavBar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      </>
+    )}
+    </>
   )
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
-const NavBar = () => {
+const NavBar = ({menuOpen, setMenuOpen}) => {
+
+  useEffect(() => {
+    document.body.style.overflow = menuOpen ? 'hidden' : ''
+  }, [menuOpen])
+
   return (
-    <nav className="bg-[#161a1d]/50 px-5 text-white py-4 flex justify-between mx-auto max-w-2xl fixed top-0 left-0 right-0 z-10 backdrop-blur-lg rounded">
-      <h1 className="text-2xl font-bold"><a href="/">fazxie</a><span className='text-xl font-bold text-blue-500'>.</span></h1>
-      <ul className="flex gap-7">
-        <li><a href="/" className='hover:text-blue-500 hover:transition-colors text-lg'>Home</a></li>
-        <li><a href="/work" className='hover:text-blue-500 hover:transition-colors text-lg'>Work</a></li>
-        <li><a href="/project" className='hover:text-blue-500 hover:transition-colors text-lg'>projects</a></li>
-      </ul>
-    </nav>
-  )
-}
+    <nav className="fixed top-0 w-full z-40 bg-[#161a1d] backdrop-blur-lg">
+      <div className="max-w-2xl mx-auto px-4">
+        <div className="flex justify-between items-center h-16">
 
-export default NavBar
+          {/* fazxie :> */}
+          <a href="#home" className="font-mono text-2xl font-bold text-white">
+            fazxie<span className="text-orange-200">.</span>
+          </a>
+
+          {/* navbar content */}
+          <div className="w-8 h-5 relative cursor-pointer z-40 md:hidden" onClick={() => setMenuOpen((prev) => !prev)}>
+            &#9776;
+          </div>
+
+          <div className="hidden md:flex items-center space-x-6">
+            <a href="#home" className="text-gray-200 hover:text-white transition-colors font-semibold text-lg">
+              Home
+            </a>
+            <a href="#work" className="text-gray-200 hover:text-white transition-colors font-semibold text-lg">
+              Work
+            </a>
+            <a href="#projects" className="text-gray-200 hover:text-white transition-colors font-semibold text-lg">
+              Projects
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
